9dc759fbe1653f4b879acc8abb050e65b81b7104
Nimbus15 <Alex.ZBP2456@GMAIL.COM>
2023-02-25 20:41:14 +0000

    finished front -end draft

---
+++ app/src/main/java/com/example/fypproject/InventoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.fypproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inventory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
---
+++ app/src/main/java/com/example/fypproject/models/Transaction.java
@@ -0,0 +1,2 @@
+package com.example.fypproject.models;public class Transaction {
+}
